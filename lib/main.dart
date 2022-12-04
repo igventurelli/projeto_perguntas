ff2e3510b5276fd852a7addcfed477935f8b39df
igventurelli <igventurelli@gmail.com>
2022-12-04 18:43:49 -0300

    feat(pergunta-1): adding pergunta 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const PerguntasApp());
+  runApp(PerguntasApp());
 }
 
 class PerguntasApp extends StatelessWidget {
-  const PerguntasApp({super.key});
+  PerguntasApp({super.key});
+
+  final perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito?'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +19,23 @@ class PerguntasApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: const Text('Body'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 1'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 2'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 3'),
+            )
+          ],
+        ),
       ),
     );
   }
